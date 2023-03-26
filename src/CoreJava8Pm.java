b844aaeb55221155159b9587929a54e316bd10cf
Prem Babu <premstudy123@gmail.com>
2023-03-26 18:38:23 +0530

    Initial commit first

---
+++ src/CoreJava8Pm.java
@@ -0,0 +1,12 @@
+public class CoreJava8Pm {
+    public static void main(String args[]){
+        System.out.println("this is java language");
+        System.out.println("java language is words sentive ");
+        System.out.println("java language is programming language");
+        System.out.println("java language is morst demanding language ");
+        System.out.println("this is java code ");
+        System.out.println("this is java language");
+        System.out.println("this is java language");
+        System.out.println("this is java language");
+    }
+}
